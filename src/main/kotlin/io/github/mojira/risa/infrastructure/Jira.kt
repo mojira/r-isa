4a5f4bcf3edce2da2a361d43cbbbad5d5fe8b733
SPGoding <15277496+SPGoding@users.noreply.github.com>
2020-11-13 20:38:13 -0600

    Fix not getting all tickets

--- src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
+++ src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
@@ -5,17 +5,14 @@ import io.github.mojira.risa.domain.Snapshot
 import io.github.mojira.risa.domain.Ticket
 import io.github.mojira.risa.domain.TicketResolution
 import io.github.mojira.risa.infrastructure.config.Risa
-import net.rcarz.jiraclient.Issue
-import net.rcarz.jiraclient.JiraClient
-import net.rcarz.jiraclient.Resolution
-import net.rcarz.jiraclient.Status
-import net.rcarz.jiraclient.TokenCredentials
-import net.rcarz.jiraclient.Version
+import net.rcarz.jiraclient.*
 import java.text.SimpleDateFormat
 import java.time.Instant
 
 private val versionDateFormat = SimpleDateFormat("yyyy-MM-dd")
 private fun String.toVersionReleaseInstant() = versionDateFormat.parse(this).toInstant()
+
+private const val MAX_RESULT = 50
 
 fun loginToJira(config: Config) = JiraClient(
     config[Risa.Credentials.Jira.url],
@@ -29,16 +26,52 @@ fun getCurrentSnapshot(jiraClient: JiraClient): Snapshot = jiraClient
     .map { Snapshot(it.name, it.releaseDate.toVersionReleaseInstant()) }
     .maxByOrNull { it.releasedDate.toEpochMilli() }!!
 
-@SuppressWarnings("MaxLineLength")
-fun getTicketsForSnapshot(jiraClient: JiraClient, currentSnapshot: Snapshot): List<Ticket> =
-    jiraClient
-        .searchIssues(
-            "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\"))",
-            "*all"
+fun getTicketsForSnapshot(jiraClient: JiraClient, currentSnapshot: Snapshot): List<Ticket> {
+    val result = emptyList<Ticket>().toMutableList()
+    val jql = getJql(currentSnapshot)
+
+    var startAt = 0
+    var wasPaginated: Boolean
+    do {
+        wasPaginated = false
+        result.addAll(
+            searchTickets(jiraClient, currentSnapshot, jql, startAt) { wasPaginated = true }
         )
+
+        startAt += MAX_RESULT
+    } while (wasPaginated)
+
+    return result
+}
+
+@SuppressWarnings("MaxLineLength")
+private fun getJql(currentSnapshot: Snapshot): String =
+    "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\"))"
+
+private fun searchTickets(
+    jiraClient: JiraClient,
+    currentSnapshot: Snapshot,
+    jql: String,
+    startAt: Int,
+    onQueryPaginated: () -> Unit
+): List<Ticket> {
+    val queryResult = jiraClient
+        .searchIssues(
+            jql,
+            "*all",
+            MAX_RESULT,
+            startAt
+        )
+
+    if (startAt + queryResult.max < queryResult.total) {
+        onQueryPaginated()
+    }
+
+    return queryResult
         .issues
         .filter { it.versions.containsAnOlderVersionThanCurrent(currentSnapshot.releasedDate) }
         .map { Ticket(it.key, it.summary, it.parseResolution(), "") }
+}
 
 fun Issue.parseResolution(): TicketResolution = if (isUnresolved(resolution) || isOpen(status)) {
     "Open"
