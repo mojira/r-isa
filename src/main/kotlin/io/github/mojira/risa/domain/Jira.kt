7e2018e8081092f4541502df40dabc593c1acecb
Uriel Salischiker <uriel@urielsalis.me>
2020-11-14 01:07:06 +0100

    Generate report!
    Fixes #6, #7, #8

--- src/main/kotlin/io/github/mojira/risa/domain/Jira.kt
+++ src/main/kotlin/io/github/mojira/risa/domain/Jira.kt
@@ -2,3 +2,12 @@ package io.github.mojira.risa.domain
 
 typealias TicketId = String
 typealias TicketTitle = String
+typealias TicketResolution = String
+typealias TicketComment = String
+
+data class Ticket(
+    val id: TicketId,
+    val title: TicketTitle,
+    val resolution: TicketResolution,
+    val comment: TicketComment
+)
--- src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
+++ src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
@@ -2,21 +2,53 @@ package io.github.mojira.risa.infrastructure
 
 import com.uchuhimo.konf.Config
 import io.github.mojira.risa.domain.Snapshot
-import io.github.mojira.risa.domain.TicketId
-import io.github.mojira.risa.domain.TicketTitle
+import io.github.mojira.risa.domain.Ticket
+import io.github.mojira.risa.domain.TicketResolution
 import io.github.mojira.risa.infrastructure.config.Risa
+import net.rcarz.jiraclient.Issue
 import net.rcarz.jiraclient.JiraClient
+import net.rcarz.jiraclient.Resolution
+import net.rcarz.jiraclient.Status
 import net.rcarz.jiraclient.TokenCredentials
+import net.rcarz.jiraclient.Version
+import java.text.SimpleDateFormat
+import java.time.Instant
+
+private val versionDateFormat = SimpleDateFormat("yyyy-MM-dd")
+private fun String.toVersionReleaseInstant() = versionDateFormat.parse(this).toInstant()
 
 fun loginToJira(config: Config) = JiraClient(
     config[Risa.Credentials.Jira.url],
     TokenCredentials(config[Risa.Credentials.Jira.username], config[Risa.Credentials.Jira.password])
 )
 
-fun getCurrentSnapshot(jiraClient: JiraClient): Snapshot {
-    TODO()
+fun getCurrentSnapshot(jiraClient: JiraClient): Snapshot = jiraClient
+    .getProject("MC")
+    .versions
+    .filter { !it.isArchived && it.isReleased }
+    .map { Snapshot(it.name, it.releaseDate.toVersionReleaseInstant()) }
+    .maxByOrNull { it.releasedDate.toEpochMilli() }!!
+
+@SuppressWarnings("MaxLineLength")
+fun getTicketsForSnapshot(jiraClient: JiraClient, currentSnapshot: Snapshot): List<Ticket> =
+    jiraClient
+        .searchIssues(
+            "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\"))",
+            "*all"
+        )
+        .issues
+        .filter { it.versions.containsAOlderVersionThanCurrent(currentSnapshot.releasedDate) }
+        .map { Ticket(it.key, it.summary, it.parseResolution(), "") }
+
+fun Issue.parseResolution(): TicketResolution = if (isUnResolved(resolution) || isOpen(status)) {
+    "Open"
+} else {
+    resolution.name
 }
 
-fun getTicketsForSnapshot(jiraClient: JiraClient, currentSnapshot: Snapshot): List<Pair<TicketId, TicketTitle>> {
-    TODO()
-}
+fun isOpen(status: Status) = status.name == "Reopened" || status.name == "Open"
+
+fun isUnResolved(resolution: Resolution?) = resolution == null || resolution.name == "Unresolved"
+
+private fun List<Version>.containsAOlderVersionThanCurrent(time: Instant) =
+    any { it.releaseDate.toVersionReleaseInstant() < time }
