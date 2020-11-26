025f73cdc4a9857f3e5119e9df9c56acf33855a0
SPGoding <15277496+SPGoding@users.noreply.github.com>
2020-11-26 13:05:05 -0600

    Escape ticket texts in tables (#35)
    * Escape ticket texts in tables
    
    * Update regex
    
    * Fun
    
    Co-authored-by: Uriel Salischiker <uriel@urielsalis.me>

--- src/main/kotlin/io/github/mojira/risa/application/ReportGenerator.kt
+++ src/main/kotlin/io/github/mojira/risa/application/ReportGenerator.kt
@@ -38,11 +38,16 @@ private fun Map<Snapshot, RedditPost>.getPreviousOf(snapshot: Snapshot): RedditP
     ?.value
     ?: ""
 
+@Suppress("MaxLineLength")
 private fun Ticket.toTableRow(): String {
     val strikethrough = resolution in listOf("Fixed", "Won't Fix", "Works As Intended")
     return if (strikethrough) {
-        "|[~~$id~~](https://bugs.mojang.com/browse/$id)|~~$title~~|$resolution|$comment\n"
+        "|[~~$id~~](https://bugs.mojang.com/browse/$id)|~~${title.escape()}~~|${resolution.escape()}|${comment.escape()}\n"
     } else {
-        "|[$id](https://bugs.mojang.com/browse/$id)|$title|$resolution|$comment\n"
+        "|[$id](https://bugs.mojang.com/browse/$id)|${title.escape()}|${resolution.escape()}|${comment.escape()}\n"
     }
 }
+
+// Special Character Reference: https://github.com/mojira/r-isa/issues/34
+private fun String.escape(): String =
+    replace("""[!"#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]""".toRegex()) { "\\${it.value}" }
