8529aef5959b7d5713be328ebb009c78b474dfd1
Uriel Salischiker <uriel@urielsalis.me>
2020-11-27 22:51:47 +0100

    Run every hour

--- src/main/kotlin/io/github/mojira/risa/Main.kt
+++ src/main/kotlin/io/github/mojira/risa/Main.kt
@@ -1,5 +1,7 @@
 package io.github.mojira.risa
 
+import arrow.fx.extensions.io.concurrent.sleep
+import arrow.fx.typeclasses.Duration
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.mojira.risa.application.generateReport
 import io.github.mojira.risa.domain.RedditPost
@@ -21,6 +23,7 @@ import io.github.mojira.risa.infrastructure.saveSnapshotPosts
 import io.github.mojira.risa.infrastructure.setWebhookOfLogger
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 val log: Logger = LoggerFactory.getLogger("Risa")
@@ -37,39 +40,43 @@ fun main() {
     val jiraClient = loginToJira(config)
     log.info("Logged in to Jira")
 
-    val snapshotPosts: Map<Snapshot, RedditPost>
-    val currentSnapshot: Snapshot
-    val previousSnapshotPost: RedditPost
-    val ticketsForSnapshot: List<Ticket>
-    try {
-        snapshotPosts = readSnapshotPosts(mapper)
-        log.info("Loaded ${snapshotPosts.size} previous snapshots")
-        currentSnapshot = getCurrentSnapshot(jiraClient)
-        log.info("Current snapshot: ${currentSnapshot.name}")
-        previousSnapshotPost = getPreviousSnapshotPost(snapshotPosts)
+    while(true) {
+        val snapshotPosts: Map<Snapshot, RedditPost>
+        val currentSnapshot: Snapshot
+        val previousSnapshotPost: RedditPost
+        val ticketsForSnapshot: List<Ticket>
+        try {
+            snapshotPosts = readSnapshotPosts(mapper)
+            log.info("Loaded ${snapshotPosts.size} previous snapshots")
+            currentSnapshot = getCurrentSnapshot(jiraClient)
+            log.info("Current snapshot: ${currentSnapshot.name}")
+            previousSnapshotPost = getPreviousSnapshotPost(snapshotPosts)
 
-        ticketsForSnapshot = getTicketsForSnapshot(jiraClient, currentSnapshot)
-        log.info("Tickets for current snapshot: ${ticketsForSnapshot.size}")
-    } catch (e: Exception) {
-        log.error("Error getting tickets from Jira", e)
-        exitProcess(1)
-    }
-
-    val report = generateReport(ticketsForSnapshot, currentSnapshot, snapshotPosts)
-
-    val currentPost: RedditPost
-    val editedPost: RedditPost
-    try {
-        currentPost = getOrCreateCurrentPost(redditCredentials, snapshotPosts, currentSnapshot)
-        editedPost = getNewOrPreviousPost(snapshotPosts, previousSnapshotPost, currentSnapshot)
-        if (editedPost == previousSnapshotPost) {
-            editPost(redditCredentials, editedPost, "This post is no longer being maintained.")
+            ticketsForSnapshot = getTicketsForSnapshot(jiraClient, currentSnapshot)
+            log.info("Tickets for current snapshot: ${ticketsForSnapshot.size}")
+        } catch (e: Exception) {
+            log.error("Error getting tickets from Jira", e)
+            exitProcess(1)
         }
-        editPost(redditCredentials, currentPost, report)
-        log.info("Posted to reddit: https://www.reddit.com/r/Mojira/comments/$currentPost")
-    } catch (e: Exception) {
-        log.error("Error posting to Reddit", e)
-        exitProcess(1)
+
+        val report = generateReport(ticketsForSnapshot, currentSnapshot, snapshotPosts)
+
+        val currentPost: RedditPost
+        val editedPost: RedditPost
+        try {
+            currentPost = getOrCreateCurrentPost(redditCredentials, snapshotPosts, currentSnapshot)
+            editedPost = getNewOrPreviousPost(snapshotPosts, previousSnapshotPost, currentSnapshot)
+            if (editedPost == previousSnapshotPost) {
+                editPost(redditCredentials, editedPost, "This post is no longer being maintained.")
+            }
+            editPost(redditCredentials, currentPost, report)
+            log.info("Posted to reddit: https://www.reddit.com/r/Mojira/comments/$currentPost")
+        } catch (e: Exception) {
+            log.error("Error posting to Reddit", e)
+            exitProcess(1)
+        }
+        saveSnapshotPosts(mapper, snapshotPosts.add(currentSnapshot, currentPost))
+
+        sleep(Duration(1, TimeUnit.HOURS))
     }
-    saveSnapshotPosts(mapper, snapshotPosts.add(currentSnapshot, currentPost))
 }
