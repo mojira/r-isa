1cf3108a96aa3fdc4eb3916db0a7ad7b439e8423
SPGoding <15277496+SPGoding@users.noreply.github.com>
2020-11-28 03:36:02 -0600

    Fix Reddit markdown formatting (#41)

--- src/main/kotlin/io/github/mojira/risa/application/ReportGenerator.kt
+++ src/main/kotlin/io/github/mojira/risa/application/ReportGenerator.kt
@@ -42,7 +42,7 @@ private fun Map<Snapshot, RedditPost>.getPreviousOf(snapshot: Snapshot): RedditP
 private fun Ticket.toTableRow(): String {
     val strikethrough = resolution in listOf("Fixed", "Won't Fix", "Works As Intended")
     return if (strikethrough) {
-        "|[~~$id~~](https://bugs.mojang.com/browse/$id)|~~${title.escape()}~~|${resolution.escape()}|${comment.escape()}\n"
+        "|[~~$id~~](https://bugs.mojang.com/browse/$id)|~~${title.trim().escape()}~~|${resolution.escape()}|${comment.escape()}\n"
     } else {
         "|[$id](https://bugs.mojang.com/browse/$id)|${title.escape()}|${resolution.escape()}|${comment.escape()}\n"
     }
@@ -50,4 +50,4 @@ private fun Ticket.toTableRow(): String {
 
 // Special Character Reference: https://github.com/mojira/r-isa/issues/34
 private fun String.escape(): String =
-    replace("""[!"#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]""".toRegex()) { "\\${it.value}" }
+    replace("""[!#&()*+./:<>\[\\\]^_`{|}~]""".toRegex()) { "\\${it.value}" }
