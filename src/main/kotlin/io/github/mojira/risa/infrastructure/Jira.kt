559fd25d65de5b1cba9e2f22e3728fa815bb73ac
Uriel Salischiker <uriel@urielsalis.me>
2020-11-29 12:10:00 +0100

    Order descending so confirmed stuff is first

--- src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
+++ src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
@@ -55,7 +55,7 @@ fun getTicketsForSnapshot(jiraClient: JiraClient, config: Config, currentSnapsho
 
 @SuppressWarnings("MaxLineLength")
 private fun getJql(currentSnapshot: Snapshot): String =
-    "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND created > ${currentSnapshot.releasedDate.minus(1L, ChronoUnit.DAYS).toEpochMilli()} AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\")) ORDER BY \"Confirmation Status\" ASC, key ASC"
+    "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND created > ${currentSnapshot.releasedDate.minus(1L, ChronoUnit.DAYS).toEpochMilli()} AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\")) ORDER BY \"Confirmation Status\" DESC, key ASC"
 
 @SuppressWarnings("LongParameterList")
 private fun searchTickets(
