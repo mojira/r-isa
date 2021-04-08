babd81c5464e078fecfeb5530bcc738016c21958
Uriel Salischiker <uriel@urielsalis.me>
2021-04-08 12:25:58 +0200

    Update src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
    Co-authored-by: violine1101 <violine1101@gmail.com>

--- src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
+++ src/main/kotlin/io/github/mojira/risa/infrastructure/Jira.kt
@@ -55,7 +55,7 @@ fun getTicketsForSnapshot(jiraClient: JiraClient, config: Config, currentSnapsho
 
 @SuppressWarnings("MaxLineLength")
 private fun getJql(currentSnapshot: Snapshot): String =
-    "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND created > ${currentSnapshot.releasedDate.minus(1L, ChronoUnit.DAYS).toEpochMilli()} AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\")) AND ((resolution != \"Awaiting Response\" OR resolution is EMPTY ) OR (resolution = \"Awaiting Response\" AND updated < 24h)) ORDER BY \"Confirmation Status\" DESC, key ASC"
+    "project = MC AND affectedVersion = \"${currentSnapshot.name}\" AND created > ${currentSnapshot.releasedDate.minus(1L, ChronoUnit.DAYS).toEpochMilli()} AND (status = Open OR status = Reopened OR resolution in (\"Works As Intended\", \"Fixed\", \"Awaiting Response\", \"Unresolved\", \"Won't Fix\")) AND ((resolution != \"Awaiting Response\" OR resolution is EMPTY ) OR (resolution = \"Awaiting Response\" AND updated > -24h)) ORDER BY \"Confirmation Status\" DESC, key ASC"
 
 @SuppressWarnings("LongParameterList")
 private fun searchTickets(
